0f6f78b89acbd1150af2861aa4e6325725cc616d
Rissamaliaaa <47821816+Rissamaliaaa@users.noreply.github.com>
2020-05-04 19:57:39 +0700

    Revert "patch iki tolong di acc yoh"

---
+++ formulir.php
@@ -0,0 +1,17 @@
+<?php 
+class formulir extends PT extends PS {
+	public $tanggalMulaiAkreditasi;
+	public $tanggalAkhirAkreditasi;
+	
+	public function displayPT($namaPT, $kodePT, $namaPS, $kodePS)
+	{
+
+	}
+	public function displayTanggal($tanggalMulaiAkreditasi, $tanggalAkhirAkreditasi)
+	{
+		
+	}
+}
+
+
+ ?>
